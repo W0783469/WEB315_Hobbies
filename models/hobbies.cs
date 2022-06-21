819410ac2c099081ad8af168e1a1c57c03a2d38e
Varinder Kaur <VK92@myscc.ca>
2022-06-21 16:02:54 -0400

    update Hobbies.cs in models

--- models/hobbies.cs
+++ models/hobbies.cs
@@ -1,11 +1,13 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WEB315_Hobbies.Models
 {
     public class hobbies
     {
         public int ID { get; set; }
+         [Display(Name = "New Title")]
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
