36114c8c7896ad5e2377332a456c23c898b36ef4
Varinder Kaur <VK92@myscc.ca>
2022-06-21 14:34:27 -0400

    updated the start up file

--- Startup.cs
+++ Startup.cs
@@ -13,48 +13,55 @@ using Microsoft.EntityFrameworkCore;
 namespace WEB315_Hobbies
 {
     public class Startup
+{
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-
-            services.AddDbContext<WEB315_HobbiesContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("WEB315_HobbiesContext")));
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
-        }
+        Environment = env;
+        Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Environment { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        if (Environment.IsDevelopment())
+        {
+            services.AddDbContext<WEB315_HobbiesContext>(options =>
+            options.UseSqlite(
+                Configuration.GetConnectionString("WEB315_HobbiesContext")));
+        }
+        else
+        {
+            services.AddDbContext<WEB315_HobbiesContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("hobbiesContext")));
+        }
+
+        services.AddRazorPages();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        if (Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();
+        });
+    }
+}
 }
