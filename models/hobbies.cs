04a0b5cfff381b2299f922a040f4470db8a1b56c
Varinder Kaur <VK92@myscc.ca>
2022-06-25 20:40:48 -0400

    validation rules added to model properties
    validation rules added to model properties

--- models/hobbies.cs
+++ models/hobbies.cs
@@ -6,16 +6,24 @@ namespace WEB315_Hobbies.Models
     public class hobbies
     {
         public int ID { get; set; }
-      
+        [StringLength(60, MinimumLength = 3)]
+       [Required]
         public string Title { get; set; }
 
-        
+        [Required]
         [DataType(DataType.Date)]
         public DateTime VisitDate { get; set; }
-      
+        [StringLength(32, MinimumLength = 4)]
+         [Required]
         public string Location { get; set; }
+        [Required]
         public decimal cost { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required]
+        
+        [StringLength(32, MinimumLength = 4)]
         public string placetype {get;set;}
+         [Range(1, 100)]
+        [Required]
         public int  visitcount {get;set;}
     }
 }
