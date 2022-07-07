a4fd565f0b0062d47e9607a9b48ca8fb995e620a
Varinder Kaur <VK92@myscc.ca>
2022-07-06 23:02:56 -0400

    new component updated
    new component updated

---
+++ Data/places.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace WEB315_Hobbies.Data
+{
+    public class places
+    {
+        public DateTime Date { get; set; }
+
+        public string location { get; set; }
+
+        public int cost { get; set; }
+
+        public string placetype { get; set; }
+    }
+    
+    }
---
+++ Data/placeservices.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB315_Hobbies.Data
+{
+public class placeservices
+    {
+ private static readonly string[] placetype = new[]
+        {
+            "adventourous", "Treking", "River rafting ", "Traditional","Listening music"
+        };
+         private static readonly string[] location = new[]
+        {
+            "Delhi", "Jalandhar", "Shimla ", "Amritsar","Japan"
+        };
+           private static readonly int[] cost = new[]
+        {
+            1000, 2000, 3000 , 4000,5000
+        };
+  public Task<places[]> GetPlacesAsync(DateTime startDate)
+  {
+  var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new places
+            {
+                Date = startDate.AddDays(index),
+                cost = cost[rng.Next(cost.Length)],
+                placetype = placetype[rng.Next(placetype.Length)],
+                location = location[rng.Next(location.Length)]
+            }).ToArray());
+        }
+}
+
+}
